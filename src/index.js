0df380b071a2fd49751cd9fe9e7dafd7912880a8
Katya <katyagilevaukr@gmail.com>
2023-04-19 00:25:52 +0300

    summary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import {App} from 'components/App/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+
+
+
+
+  
